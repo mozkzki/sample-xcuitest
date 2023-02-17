1674a4bc27a0c936e06080b36a4039e218467a8b
mozkzki <mozkzki@gmail.com>
2023-02-17 14:17:47 +0900

    不要ファイル・コード削除

--- UITestSampleTests/MainViewControllerTests.swift
+++ UITestSampleTests/MainViewControllerTests.swift
@@ -12,17 +12,13 @@ import UIKit
 
 class MainViewControllerTests: SnapshotTestCase {
 
-    func testHoge() {
-
-    }
-    
     func testMainViewSnapshot() throws {
-        print("◆◆◆◆◆◆◆◆◆◆ test")
-        let dic = ProcessInfo.processInfo.environment
-        if let value: String = dic["SNAPSHOT_TEST_RECORD_MODE"] {
-            print(value)
-        }
-        print("◆◆◆◆◆◆◆◆◆◆ test")
+//        print("◆◆◆◆◆◆◆◆◆◆ test")
+//        let dic = ProcessInfo.processInfo.environment
+//        if let value: String = dic["SNAPSHOT_TEST_RECORD_MODE"] {
+//            print(value)
+//        }
+//        print("◆◆◆◆◆◆◆◆◆◆ test")
 
         let vc = MainViewController()
         FBSnapshotVerifyView(vc.view)
